faac2e38b548fe2fbfde380c693451edea8dffb3
lohmo111 <ilohmotov05@gmail.com>
2023-04-06 20:57:59 +0300

    fixed incorrect image display

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ import requests
 from PIL import Image
 
 
-def init_geocoder_connection(toponym_to_find):
+def init_geocoder_connection(*toponym_to_find):
     geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
 
     geocoder_params = {
         "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
         "geocode": toponym_to_find,
+        # "geocode": "Australia",
         "format": "json"}
 
     response = requests.get(geocoder_api_server, params=geocoder_params)
-
+    print(response.url)
     if not response:
         print("Error connecting to geocoder")
         return -1
@@ -21,8 +22,16 @@ def init_geocoder_connection(toponym_to_find):
     return response
 
 
+def coords_creation(response):
+    json_response = response.json()
+    coords_response = json_response["response"]["GeoObjectCollection"][
+        "featureMember"][0]["GeoObject"]["Point"]["pos"]
+    coords = ",".join([str(elem) for elem in coords_response.split()])
+    return coords
+
+
 def calculate_spn(lower_corner, upper_corner):
-    spn = [round(upper_corner[0] - lower_corner[0], 6), round(upper_corner[1] - lower_corner[1], 6)]
+    spn = [round(upper_corner[0] - lower_corner[0], 6), abs(round(upper_corner[1] - lower_corner[1], 6))]
     return spn
 
 
@@ -31,7 +40,6 @@ def find_toponym_spn(response):
 
     toponym = json_response["response"]["GeoObjectCollection"][
         "featureMember"][0]["GeoObject"]
-
     borders = toponym['boundedBy']['Envelope']
     lower_corner = list(map(float, borders['lowerCorner'].split()))
     upper_corner = list(map(float, borders['upperCorner'].split()))
@@ -44,7 +52,7 @@ def create_image_response(coords, spn):
     map_params = {
         "ll": coords,
         "spn": ",".join([str(elem) for elem in spn]),
-        "l": "map",
+        "l": "map"
     }
 
     map_api_server = "http://static-maps.yandex.ru/1.x/"
@@ -55,8 +63,9 @@ def create_image_response(coords, spn):
 
 
 if __name__ == "__main__":
-    coords = "37.60519,55.82307"
-    geocoder_response = init_geocoder_connection(coords)
+    toponym = "37.60519,55.82307"
+    geocoder_response = init_geocoder_connection(toponym)
+    coords = coords_creation(geocoder_response)
     spn = find_toponym_spn(geocoder_response)
 
     image = create_image_response(coords, spn)
